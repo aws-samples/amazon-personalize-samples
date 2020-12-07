71af6ba0cb8f6b9208eaaefdcbbbba6cf72b4b61
Igor <igor.fcb@gmail.com>
2020-12-07 08:39:41 -0800

    added delete all resources step function + lambdas

---
+++ next_steps/operations/ml_ops/personalize-step-functions/lambdas/delete-campaign/delete-campaign.py
@@ -0,0 +1,11 @@
+from os import environ
+from loader import Loader
+import actions
+
+LOADER = Loader()
+
+
+def lambda_handler(event, context):
+    status = LOADER.personalize_cli.delete_campaign(
+        campaignArn=event['campaignArn']
+    )
